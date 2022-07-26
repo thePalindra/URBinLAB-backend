670dc08762edc9db268182d5c95217cf5c40e5f2
Palinda <rjp.goncalves@campus.fct.unl.pt>
2022-07-26 17:53:13 +0100

    Token errors from the frontend to the backend

--- backend/src/main/java/com/URBinLAB/services/DocumentService.java
+++ backend/src/main/java/com/URBinLAB/services/DocumentService.java
@@ -43,6 +43,7 @@ public class DocumentService {
             if (!map.containsKey("token"))
                 return AccessControl.access(feature, "all");
 
+            System.out.println(map.get("token").toString());
             String token = map.get("token").toString();
             token = token.substring(1, token.length() - 1);
             Token temp = gson.fromJson(token, Token.class);
